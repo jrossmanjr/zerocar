5249caf70b083944cc1eaac087fa833764b3b4ce
jrossmanjr <jon.rossman.jr@gmail.com>
2016-06-04 21:28:56 -0400

    Update ZeroCar.sh
    edited wifi settings

--- ZeroCar.sh
+++ ZeroCar.sh
@@ -66,7 +66,7 @@ function install_wifi() {
 	echo "::: Installing wifi drivers"
 	wget https://dl.dropboxusercontent.com/u/80256631/install-wifi.tar.gz
 	tar xzf install-wifi.tar.gz
-	$SUDO ./install-wifi.sh
+	$SUDO ./install-wifi
 	echo " DONE!"
 }
 
@@ -155,26 +155,26 @@ DAEMON_CONF="/etc/hostapd/hostapd.conf"' | sudo tee --append /etc/default/hostap
   	
 	$SUDO cp /etc/network/interfaces /etc/network/interfaces.bkp
   	$SUDO echo 'source-directory /etc/network/interfaces.d
+auto lo
+iface lo inet loopback
 
-	auto lo
-	iface lo inet loopback
+iface eth0 inet manual
 
-	iface eth0 inet manual
-
-	iface wlan0 inet static
-  		address 10.0.0.1
-  		netmask 255.255.255.0' > /etc/network/interfaces
+iface wlan0 inet static
+address 10.0.0.1
+netmask 255.255.255.0' > /etc/network/interfaces
 	$SUDO echo 'interface=wlan0       # the interface used by the AP
 hw_mode=g             # g simply means 2.4GHz band
-channel=10            # the channel to use
-ieee80211d=1          # limit the frequencies used to those allowed in the country
-country_code=US       # the country code
+channel=11            # the channel to use
+#ieee80211d=1          # limit the frequencies used to those allowed in the country
+#country_code=US       # the country code
 ieee80211n=1          # 802.11n support
 wmm_enabled=1         # QoS support
 auth_algs=1           # 1=wpa, 2=wep, 3=both
 wpa=2                 # WPA2 only
 wpa_key_mgmt=WPA-PSK  
 rsn_pairwise=CCMP' > /etc/hostapd/hostapd.conf
+	echo ":::"
 	echo "::: Give your WiFi a name: "
 	read var2
 	echo "::: The WiFi will be called:  $var2"
