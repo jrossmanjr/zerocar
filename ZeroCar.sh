d55f80a5f9fb2abfe8bda47eb62613e7ca908fa4
jrossmanjr <jon.rossman.jr@gmail.com>
2016-06-04 22:24:10 -0400

    Update ZeroCar.sh
    gave thanks

--- ZeroCar.sh
+++ ZeroCar.sh
@@ -6,8 +6,9 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-# shoutout to the folks making PiHole & PIRATEBOX for showing me the way and essentially teaching me to code for the Pi...
+# shoutout to the folks making PiHole, Adafruit, & PIRATEBOX for showing me the way and essentially teaching me to code for the Pi...
 # thanks to MrEngman for making the wifi installer!! more info: https://www.raspberrypi.org/forums/viewtopic.php?p=462982
+# alot of help came from ADAFRUIT: https://learn.adafruit.com/setting-up-a-raspberry-pi-as-a-wifi-access-point/install-software
 
 # Run this script as root or under sudo
 echo ":::
