fa7e6c4bb7f52fca57990c71354d3b4e15780df2
jrossmanjr <jon.rossman.jr@gmail.com>
2016-10-17 00:49:30 -0400

    Update ZeroCar-DroppyTest.sh

--- ZeroCar-DroppyTest.sh
+++ ZeroCar-DroppyTest.sh
@@ -217,7 +217,7 @@ function install_droppy() {
   # update Node.js, NPM and install droppy to allow for web file serving
   echo ":::"
   echo "::: It is finished... restarting."
-  $SUDO apt intstall -y node npm
+  $SUDO apt-get intstall -y node npm
   $SUDO npm cache clean -f && sudo npm install -g n
   $SUDO n stable
   $SUDO npm install -g droppy
