8a32f9b896f1e9b3117ff67fce839b6cf1298012
jrossmanjr <jon.rossman.jr@gmail.com>
2016-10-21 01:55:52 -0400

    Update ChipCar.sh

--- ChipCar.sh
+++ ChipCar.sh
@@ -2,7 +2,7 @@
 # ChipCar Install Script
 # (c) 2016 by jrossmanjr
 # Use a C.H.I.P. as a WiFi hotspot to serve up files
-# RaspiCar is free software: you can redistribute it and/or modify
+# ChipCar is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
