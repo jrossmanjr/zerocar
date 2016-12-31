aeaa10a996a225d81add215523b4429b22604954
jrossmanjr <jon.rossman.jr@gmail.com>
2016-12-31 15:17:22 -0500

    Update ChipCar.sh

--- ChipCar.sh
+++ ChipCar.sh
@@ -249,7 +249,7 @@ function fix_startup() {
   # restart the wifi as last function on startup
   echo ":::"
   echo "::: Fixing the wifi at startup"
-  &SUDO apt-get install -y wireless-tools
+  $SUDO apt-get install -y wireless-tools
   $SUDO cp chip.local /etc/rc.local
   $SUDO chmod +x /etc/rc.local
   echo "::: DONE!"
