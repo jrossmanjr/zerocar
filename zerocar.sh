3ec9b058639f948aaf3c761b8ca4d0c4c96c1a67
jrossmanjr <8453312+jrossmanjr@users.noreply.github.com>
2020-02-27 22:41:54 -0500

    Update wifi driver installer

--- zerocar.sh
+++ zerocar.sh
@@ -101,7 +101,7 @@ function install_wifi() {
 # installing wifi drivers for rtl8188eu chipset
   echo ":::"
   echo "::: Installing wifi drivers"
-  $SUDO wget http://www.fars-robotics.net/install-wifi -O /usr/bin/install-wifi
+  $SUDO wget http://downloads.fars-robotics.net/wifi-drivers/install-wifi -O /usr/bin/install-wifi
   $SUDO chmod +x /usr/bin/install-wifi
   $SUDO install-wifi
   echo "::: DONE!"
